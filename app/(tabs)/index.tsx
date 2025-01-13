ff273cfdc018ee9bc2cfe85e86c3222385bd1c37
Namkhang Le <LENN@gatech.edu>
2025-01-13 18:02:30 -0500

    Update instruction text in HomeScreen component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -36,11 +36,7 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Click Explore 2 Learn More</ThemedText>
         <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+          When you're ready, click Explore to get your life started.
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
