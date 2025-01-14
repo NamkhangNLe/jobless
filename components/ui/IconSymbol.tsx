e8d0571fe990623ae40c7666560c87dfe06343ea
Namkhang Le <LENN@gatech.edu>
2025-01-14 16:12:59 -0500

    Add Adventure tab to TabLayout with corresponding icon

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -13,6 +13,7 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'adventureIcon': 'send', // Replace 'YOUR_ICON_NAME' with the actual icon name
 } as Partial<
   Record<
     import('expo-symbols').SymbolViewProps['name'],
